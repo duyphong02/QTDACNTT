97757311001b1254195d48d47e1eb66cc9074876
duyphong02 <115386674+duyphong02@users.noreply.github.com>
2023-05-18 23:18:17 +0700

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QLBXE.Startup))]
+namespace QLBXE
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
